89d22c06fd202ca1cb2a78d1bc4bdbb6597eb4fe
Sam Dawson <sam.dawson181@gmail.com>
2019-06-30 15:25:54 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+
+export default StickyHeaderWatcher = (props) => {
+  const [isStuck, setIsStuck] = useState(false)
+  const sentinalEl = React.createRef()
+  
+  useEffect(() => {
+    const handler = (entries) => {
+      setIsStuck(!entries[0].isIntersecting)
+    }
+
+    const observer = new window.IntersectionObserver(handler)
+    observer.observe(sentinalEl.current)
+  }, [])
+
+  return (
+    <React.Fragment>
+      <div ref={sentinalEl} />
+      {props.render({isStuck})}
+    </React.Fragment>
+  )
+}
